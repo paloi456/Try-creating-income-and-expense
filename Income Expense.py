d6abb062777babdab79ce0dd68225287fa75903a
paloi456 <paloi456@hotmail.com>
2024-09-05 11:35:22 +0700

    Add files via upload
    v.2

--- Income Expense.py
+++ Income Expense.py
@@ -1,27 +1,40 @@
-Balance = 0
-Income  = []
-Expense = []
+Balance        = 0
+Income         = []
+Income_source  = []
+Expense        = []
+Expense_source = []
+
 while True:
     print("Total Balance ", Balance)
     print("Type 1 for add income ")
     print("Type 2 for add expense ")
-    print("Type 3 for exit ")
+    print("Type 3 for check income ")
+    print("Type 4 for check expense ")
+    print("Type 5 for exit ")
 
     choice = int(input("Enter "))    
 
 
     if choice == 1:
-        income, source = input("Enter Income and what for ").split()
-        income       = int(income)
-        source       = str(source)
-        Balance      = Balance + income
+        income, Income_source = input("Enter Income and source seperate by space ").split()
+        income                = int(income)
+        Income_source         = str(Income_source)
+        Balance              += income
         print(Balance)
+
     elif choice == 2:
-        Expense, Transaction = input("Enter Income and what for ").split()
-        Expense      = int(Expense)
-        Transaction  = str(Transaction)
-        Balance      = Balance - Expense
+        Expense, Expense_source = input("Enter Income and source seperate by space ").split()
+        Expense                 = int(Expense)
+        Expense_source          = str(Expense_source)
+        Balance                -= Expense
         print(Balance)
+
+    elif choice == 3:
+        print(Income," ", Income_source)
+
+    elif choice == 4:
+        print(Expense," ", Expense_source)
+
     else:
         break
 
